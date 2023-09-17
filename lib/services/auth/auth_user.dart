f975426df2c509ddbe4a5e2fad407c826b489764
Kalzud <udumakalz@gmail.com>
2023-09-17 14:14:44 +0100

    v-10.7.2

--- lib/services/auth/auth_user.dart
+++ lib/services/auth/auth_user.dart
@@ -5,11 +5,16 @@ import 'package:flutter/material.dart';
 //is seen so we want to get only the user
 @immutable
 class AuthUser {
+  final String id;
   final String? email;
   //creaing boolean varibale that would return true or false
   final bool isEmailVerified;
   // initialise the variable in constructor
-  const AuthUser({required this.email, required this.isEmailVerified});
+  const AuthUser({
+    required this.id,
+    required this.email,
+    required this.isEmailVerified,
+  });
 
   //This factory is a constructor used to create instances of objcets and
   //in this case it is creating an instance of the objcet Auth user.
@@ -18,6 +23,9 @@ class AuthUser {
   //specified field argumnet of emailVerified from the firebase user
   //so basically creating an AuthUser object with the emailVerified and email from
   //User as an argument.
-  factory AuthUser.fromFirebase(User user) =>
-      AuthUser(isEmailVerified: user.emailVerified, email: user.email);
+  factory AuthUser.fromFirebase(User user) => AuthUser(
+        isEmailVerified: user.emailVerified,
+        email: user.email,
+        id: user.uid,
+      );
 }
