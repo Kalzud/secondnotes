e96f87af8a08674db126b9c8ac5bc8d863a3c5c0
Kalzud <udumakalz@gmail.com>
2023-09-18 19:54:36 +0100

    v-10.8.2

--- lib/services/cloud/firebase_cloud_storage.dart
+++ lib/services/cloud/firebase_cloud_storage.dart
@@ -21,14 +21,26 @@ class FirebaseCloudStorage {
 
   Future<Iterable<CloudNote>> getNotes({required String ownerUserId}) async {
     try {
-      notes
+      return await notes
           .where(
             ownerUserIdFieldName,
             isEqualTo: ownerUserId,
           )
-          .get();
+          .get()
+          .then(
+            (value) => value.docs.map((doc) => CloudNote(
+                  documentId: doc.id,
+                  ownerUserId: doc.data()[ownerUserIdFieldName] as String,
+                  text: doc.data()[textFieldName] as String,
+                )),
+          );
     } catch (e) {
       throw CouldNotGetAllNotesException();
     }
   }
+
+  Stream<Iterable<CloudNote>> getAllNotes({required String ownerUserId}) =>
+      notes.snapshots().map((event) => event.docs
+          .map((doc) => CloudNote.fromSnapshot(doc))
+          .where((note) => note.ownerUserId == ownerUserId));
 }
