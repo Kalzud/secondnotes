1fb499ff5d0f7e401009d01ad6bbcdfb2dff8060
Kalzud <udumakalz@gmail.com>
2023-09-18 18:33:14 +0100

    v-10.8.1

--- lib/services/cloud/firebase_cloud_storage.dart
+++ lib/services/cloud/firebase_cloud_storage.dart
@@ -1,9 +1,34 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:secondnotes/services/cloud/cloud.note.dart';
+import 'package:secondnotes/services/cloud/cloud_storage.dart';
+import 'package:secondnotes/services/cloud/cloud_storage_constants.dart';
 
 class FirebaseCloudStorage {
   final notes = FirebaseFirestore.instance.collection('notes');
+
+  // create and get an instance of this so there is only one instance
   static final FirebaseCloudStorage _shared =
       FirebaseCloudStorage._sharedInstance();
   FirebaseCloudStorage._sharedInstance();
   factory FirebaseCloudStorage() => _shared;
+
+  void createNotes({required String ownerUserId}) async {
+    await notes.add({
+      ownerUserIdFieldName: ownerUserId,
+      textFieldName: '',
+    });
+  }
+
+  Future<Iterable<CloudNote>> getNotes({required String ownerUserId}) async {
+    try {
+      notes
+          .where(
+            ownerUserIdFieldName,
+            isEqualTo: ownerUserId,
+          )
+          .get();
+    } catch (e) {
+      throw CouldNotGetAllNotesException();
+    }
+  }
 }
