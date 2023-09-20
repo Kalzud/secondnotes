18c7bde5026815bfd708cc05af05a840859d7ada
Kalzud <udumakalz@gmail.com>
2023-09-20 22:36:21 +0100

    v-10.11.1

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:secondnotes/constants/routes.dart';
 import 'package:secondnotes/notes/create_update_note_view.dart';
-import 'package:secondnotes/services/auth/auth_service.dart';
-import 'package:secondnotes/views/login_view.dart';
 import 'package:secondnotes/notes/notes_view.dart';
+import 'package:secondnotes/views/login_view.dart';
 import 'package:secondnotes/views/registration_view.dart';
 import 'package:secondnotes/views/verify_email_view.dart';
 
@@ -31,30 +30,80 @@ void main() async {
   );
 }
 
-class HomePage extends StatelessWidget {
+// class HomePage extends StatelessWidget {
+//   const HomePage({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return FutureBuilder(
+//       future: AuthService.firebase().initialise(),
+//       builder: (context, snapshot) {
+//         switch (snapshot.connectionState) {
+//           case ConnectionState.done:
+//             final user = AuthService.firebase().currentUser;
+//             if (user != null) {
+//               if (user.isEmailVerified) {
+//                 return const NoteView();
+//               } else {
+//                 return const VerifyEmailView();
+//               }
+//             } else {
+//               return const LoginView();
+//             }
+//           default:
+//             return const CircularProgressIndicator();
+//         }
+//       },
+//     );
+//   }
+// }
+
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-      future: AuthService.firebase().initialise(),
-      builder: (context, snapshot) {
-        switch (snapshot.connectionState) {
-          case ConnectionState.done:
-            final user = AuthService.firebase().currentUser;
-            if (user != null) {
-              if (user.isEmailVerified) {
-                return const NoteView();
-              } else {
-                return const VerifyEmailView();
-              }
-            } else {
-              return const LoginView();
-            }
-          default:
-            return const CircularProgressIndicator();
-        }
-      },
-    );
+    return const Placeholder();
   }
 }
+
+@immutable
+abstract class CounterState {
+  final int value;
+  const CounterState(this.value);
+}
+
+class CounterStateValid extends CounterState {
+  const CounterStateValid(int value) : super(value);
+}
+
+class CounterStateInvalidNumber extends CounterState {
+  final int invalidValue;
+  const CounterStateInvalidNumber({
+    required this.invalidValue,
+    required int prevousValue,
+  }) : super(prevousValue);
+}
+
+@immutable
+abstract class CounterEvent {
+  final String value;
+  const CounterEvent(this.value);
+}
+
+class IncreamentEvent extends CounterEvent {
+  const IncreamentEvent(String value) : super(value);
+}
+
+class DecreamentEvent extends CounterEvent {
+  const DecreamentEvent(String value) : super(value);
+}
+
+// class CounterBloc extends Bloc<>{
+
+// }
